90f010546ecca836f0ebc7fb3c3889d4fb76a2ad
Cassidy Ng <gccn212@gmail.com>
2022-12-07 16:01:05 -0600

    sq starts blinking

--- game.js
+++ game.js
@@ -1,26 +1,28 @@
 let userClicked = [];
 let correctAnswer = [];
-let colors = ["mint", "cyan", "blurple", "lilac"];
+// const colors = document.querySelector(".button-grid");
 const h1 = document.querySelector("h1");
 
-//let startGame = true;
-h1.addEventListener("keydown", () => {
+// let startGame = true;
+document.addEventListener("keydown", () => {
     // if(startGame==true) {
         h1.innerHTML="GO!";
-        //startGame=false;
+        // startGame=false;
         store();
      }
 );
 
 //generate random pattern of colors//
+
 let choose = Math.floor(Math.random()*4);
 const store = () => {
+    let colors = ["mint", "cyan", "blurple", "lilac"];
     document.getElementById(colors[choose]).classList.add("add");
     let audio = new Audio("beep.mp3");
     audio.play();
-    correctAnswer.push(color[choose]);
+    correctAnswer.push(colors[choose]);
 
     setTimeout(() => {
-    document.getElementById(color[choose]).classList.remove("add");
-    }, 200);
-}
+    document.getElementById(colors[choose]).classList.remove("add");
+    }, 3000);
+};
